85f42fc8037fc8bb5c60700cd08ce2b8eefcd904
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2022-09-19 17:29:33 -0300

    métodos sem retorno

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -7,5 +7,25 @@ namespace bytebank{
         public int numero_agencia;
         public string nome_agencia;
         public double saldo;
+
+        // Método(comportamento de uma classe) sacar
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor){
+                return false;
+            }
+            if(valor < 0){
+                return false;
+            }else{
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        // Método(void) não retorna valor, só é executado
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ class Program{
         conta1.nome_agencia = "Agência Central";
         conta1.saldo = 100;
 
-        // ContaCorrente conta2 = new ContaCorrente();
-        // conta2.titular = "Isadora Garcia";
-        // conta2.conta = "111999-X";
-        // conta2.numero_agencia = 58;
-        // conta2.nome_agencia = "Agência Central";
-        // conta2.saldo = 100;
+        ContaCorrente conta2 = new ContaCorrente();
+        conta2.titular = "Isadora Garcia";
+        conta2.conta = "111999-X";
+        conta2.numero_agencia = 58;
+        conta2.nome_agencia = "Agência Central";
+        conta2.saldo = 100;
 
         // Console.WriteLine("Titular: " + conta1.titular);
         // Console.WriteLine("Conta: " + conta1.conta);
@@ -29,6 +29,17 @@ class Program{
         double saldo = 100;
         double saldo2 = conta1.saldo;
         Console.WriteLine(saldo == saldo2); 
+
+        // Tipagem por referência
+        // conta1 = conta2; // aponta para o mesmo endereço de memória, por isso agora se comparadas o resultado será TRUE
+
+        Console.WriteLine("Saldo atual: " + conta1.saldo);
+        bool saque = conta1.Sacar(50);
+        Console.WriteLine("Saque realizado? " + saque);
+        Console.WriteLine("Saldo pós saque: " + conta1.saldo);
+
+        conta1.Depositar(60);
+        Console.WriteLine("Saldo pós deposito: " + conta1.saldo);
 
         Console.ReadKey();
     }
